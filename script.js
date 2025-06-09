b217e73f67cb2313aa72c482efcaf3b643079bce
Enoque Sales Silva <enoquesales52@gmail.com>
2025-06-09 18:49:22 -0300

    primeiro commit do site pessoal

---
+++ script.js
@@ -0,0 +1,4 @@
+function MostrarMensagem(){
+    const msg= document.getElementById("Mensagem");
+    msg.textContent= "Voce é capaz de conquistar o que quiser, só precisa continuar caminhando!";
+}
